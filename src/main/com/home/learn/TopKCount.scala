1326e0589d1d790574675dce4a9eb6fb0fbbbde8
mmm713 <mmm713@users.noreply.github.com>
2020-02-26 22:22:45 -0800

    implement topk
    both sql and dataset version

---
+++ src/main/com/home/learn/TopKCount.scala
@@ -0,0 +1,36 @@
+package com.home.learn
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.functions._
+
+case class Record(user: String, word: String, count: Int)
+
+object TopKCount {
+    def main(args: Array[String]): Unit = {
+        val spark: SparkSession = SparkSession.builder()
+            .master("local[*]")
+            .appName("WordCount")
+            .getOrCreate()
+        import spark.implicits._
+        val records = spark.read.format("csv")
+            .option("header", "true")
+            .load("src/main/resources/topk.csv")
+            .withColumn("count", 'Count.cast(IntegerType))
+            .as[Record]
+        val recSum = records.groupBy($"user", $"word").agg(sum("count") as "count")
+        val orderWindow = Window.partitionBy("user").orderBy(desc("count"))
+        val topKCount = recSum.withColumn("rank", rank over orderWindow).filter($"rank" < 3)
+        topKCount.drop("rank").show()
+
+        records.createOrReplaceTempView("records")
+        val result = records.sqlContext
+            .sql("select user, word, count from (select user, word, sum(count) as count, " +
+                "dense_rank() OVER (PARTITION BY user ORDER BY sum(count) DESC) as rank " +
+                "from records " +
+                "group by user, word) where rank < 3")
+        result.show()
+        spark.stop()
+    }
+}
--- src/main/com/home/learn/WordCount.scala
+++ src/main/com/home/learn/WordCount.scala
@@ -1,5 +1,20 @@
 package com.home.learn
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
 object WordCount {
-  
+    def main(args: Array[String]): Unit = {
+        val spark: SparkSession = SparkSession.builder()
+            .master("local[*]")
+            .appName("WordCount")
+            .getOrCreate()
+        import spark.implicits._
+        val wordCount = spark.read.text("src/main/resources/word.txt").as[String]
+        val wc = wordCount.flatMap(_.split(" ")).groupByKey(_.toString).count().sort(desc("count(1)"))
+        wc.show()
+        wc.limit(10).show()
+        spark.stop()
+    }
+
 }
