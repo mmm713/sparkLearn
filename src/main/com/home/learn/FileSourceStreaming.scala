7ab0799339e03ff38d275796b0f06622c1ff3bf8
mmm713 <mmm713@users.noreply.github.com>
2020-02-28 22:15:44 -0800

    add unit test
    test

--- src/main/com/home/learn/FileSourceStreaming.scala
+++ src/main/com/home/learn/FileSourceStreaming.scala
@@ -1,19 +1,30 @@
 package home.learn
 
+import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 object FileSourceStreaming {
     def main(args: Array[String]): Unit = {
-
         val spark: SparkSession = SparkSession
             .builder()
             .master("local[*]")
             .appName("FileSourceStreaming")
             .getOrCreate()
-        val userSchema: StructType = new StructType()
-            .add("name", StringType)
-            .add("sex", StringType)
-            .add("age", IntegerType)
+        val adSchema = StructType(StructField("pubId", StringType)::StructField("bidderId", StringType)
+            ::StructField("imps", IntegerType)::StructField("supplyCost", IntegerType)::Nil)
+        val df: sql.DataFrame = spark.readStream
+            .format("csv")
+            .option("header", "true")
+            .schema(adSchema)
+            .load("src/main/resources/filestream/")
+
+        df.writeStream
+            .format("console")
+            .outputMode("update")
+            .trigger(Trigger.ProcessingTime(2000))
+            .start()
+            .awaitTermination()
     }
 }
