046a4453843bcc2ebcd123b1a6ed7fcae11d1d28
weiwang-houzz <wei.wang@houzz.com>
2020-07-28 14:11:38 -0700

    update test

--- test/main/com/home/learn/StackSpec.scala
+++ test/main/com/home/learn/StackSpec.scala
@@ -1,12 +1,13 @@
 package com.home.learn
 
-import collection.mutable.Stack
 import org.scalatest._
+
+import scala.collection.mutable
 
 class StackSpec extends FlatSpec {
 
     "A Stack" should "pop values in last-in-first-out order" in {
-        val stack = new Stack[Int]
+        val stack = new mutable.Stack[Int]
         stack.push(1)
         stack.push(2)
         assert(stack.pop() === 2)
@@ -14,7 +15,7 @@ class StackSpec extends FlatSpec {
     }
 
     it should "throw NoSuchElementException if an empty stack is popped" in {
-        val emptyStack = new Stack[String]
+        val emptyStack = new mutable.Stack[String]
         assertThrows[NoSuchElementException] {
             emptyStack.pop()
         }
